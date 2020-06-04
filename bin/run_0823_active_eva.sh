7a1f888c0859a68e274a236aad0854661ce16fa6
Wei Zhang <X-Spirit.zhang@ttu.edu>
2020-06-04 11:10:54 -0400

    grammar check

---
+++ bin/run_0823_active_eva.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -N 20
+#SBATCH -C haswell
+#SBATCH -q premium
+#SBATCH -J user_activity_analyzer
+#SBATCH --mail-user=x-spirit.zhang@ttu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -t 10:00:00
+
+module load python
+conda activate ActiveDR_env
+
+srun -n 20 -c 64 --cpu_bind=cores python -u user_activity_analyzer_mpi.py -d 20160823
